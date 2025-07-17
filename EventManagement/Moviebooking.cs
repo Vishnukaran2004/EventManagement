106a91ba981b4471d42d86019efed8184e91d173
Yadushan2004 <maniyaryadu2004@gmail.com>
2025-07-17 23:54:19 +0530

    moviebooking

--- EventManagement/Moviebooking.cs
+++ EventManagement/Moviebooking.cs
@@ -16,5 +16,15 @@ namespace EventManagement
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
