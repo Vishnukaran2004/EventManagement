e8d9041c5de54f5fa303c3e89cf05b22b4318c8d
Yadushan2004 <maniyaryadu2004@gmail.com>
2025-07-17 22:44:04 +0530

    movie

--- EventManagement/Movies.Designer.cs
+++ EventManagement/Movies.Designer.cs
@@ -28,35 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btncam = new System.Windows.Forms.Button();
+            this.btnshi = new System.Windows.Forms.Button();
+            this.btnmush = new System.Windows.Forms.Button();
+            this.btnai = new System.Windows.Forms.Button();
+            this.btnkash = new System.Windows.Forms.Button();
+            this.btnjan = new System.Windows.Forms.Button();
+            this.btnloc = new System.Windows.Forms.Button();
+            this.btnape = new System.Windows.Forms.Button();
+            this.btnmoon = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btncam
             // 
-            this.button1.Location = new System.Drawing.Point(133, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btncam.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncam.Location = new System.Drawing.Point(80, 58);
+            this.btncam.Name = "btncam";
+            this.btncam.Size = new System.Drawing.Size(160, 80);
+            this.btncam.TabIndex = 0;
+            this.btncam.Text = "CAMATHAAR";
+            this.btncam.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnshi
             // 
-            this.button2.Location = new System.Drawing.Point(362, 254);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 33);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnshi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnshi.Location = new System.Drawing.Point(320, 58);
+            this.btnshi.Name = "btnshi";
+            this.btnshi.Size = new System.Drawing.Size(160, 80);
+            this.btnshi.TabIndex = 2;
+            this.btnshi.Text = "SHI - YAM";
+            this.btnshi.UseVisualStyleBackColor = true;
+            // 
+            // btnmush
+            // 
+            this.btnmush.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmush.Location = new System.Drawing.Point(560, 58);
+            this.btnmush.Name = "btnmush";
+            this.btnmush.Size = new System.Drawing.Size(160, 80);
+            this.btnmush.TabIndex = 3;
+            this.btnmush.Text = "MUSHROOM";
+            this.btnmush.UseVisualStyleBackColor = true;
+            // 
+            // btnai
+            // 
+            this.btnai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnai.Location = new System.Drawing.Point(80, 194);
+            this.btnai.Name = "btnai";
+            this.btnai.Size = new System.Drawing.Size(160, 80);
+            this.btnai.TabIndex = 4;
+            this.btnai.Text = "AI BABY";
+            this.btnai.UseVisualStyleBackColor = true;
+            this.btnai.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnkash
+            // 
+            this.btnkash.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnkash.Location = new System.Drawing.Point(80, 328);
+            this.btnkash.Name = "btnkash";
+            this.btnkash.Size = new System.Drawing.Size(160, 80);
+            this.btnkash.TabIndex = 5;
+            this.btnkash.Text = "KASHMIRAA";
+            this.btnkash.UseVisualStyleBackColor = true;
+            // 
+            // btnjan
+            // 
+            this.btnjan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjan.Location = new System.Drawing.Point(320, 194);
+            this.btnjan.Name = "btnjan";
+            this.btnjan.Size = new System.Drawing.Size(160, 80);
+            this.btnjan.TabIndex = 6;
+            this.btnjan.Text = "JANA NAYAKAYA";
+            this.btnjan.UseVisualStyleBackColor = true;
+            // 
+            // btnloc
+            // 
+            this.btnloc.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnloc.Location = new System.Drawing.Point(320, 328);
+            this.btnloc.Name = "btnloc";
+            this.btnloc.Size = new System.Drawing.Size(160, 80);
+            this.btnloc.TabIndex = 7;
+            this.btnloc.Text = "LOCAL FROCK";
+            this.btnloc.UseVisualStyleBackColor = true;
+            // 
+            // btnape
+            // 
+            this.btnape.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnape.Location = new System.Drawing.Point(560, 194);
+            this.btnape.Name = "btnape";
+            this.btnape.Size = new System.Drawing.Size(160, 80);
+            this.btnape.TabIndex = 8;
+            this.btnape.Text = "APE LANKA 2";
+            this.btnape.UseVisualStyleBackColor = true;
+            // 
+            // btnmoon
+            // 
+            this.btnmoon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmoon.Location = new System.Drawing.Point(560, 328);
+            this.btnmoon.Name = "btnmoon";
+            this.btnmoon.Size = new System.Drawing.Size(160, 80);
+            this.btnmoon.TabIndex = 9;
+            this.btnmoon.Text = "THE MOON";
+            this.btnmoon.UseVisualStyleBackColor = true;
             // 
             // Movies
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnmoon);
+            this.Controls.Add(this.btnape);
+            this.Controls.Add(this.btnloc);
+            this.Controls.Add(this.btnjan);
+            this.Controls.Add(this.btnkash);
+            this.Controls.Add(this.btnai);
+            this.Controls.Add(this.btnmush);
+            this.Controls.Add(this.btnshi);
+            this.Controls.Add(this.btncam);
             this.Name = "Movies";
             this.Text = "Movies";
             this.ResumeLayout(false);
@@ -65,7 +153,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btncam;
+        private System.Windows.Forms.Button btnshi;
+        private System.Windows.Forms.Button btnmush;
+        private System.Windows.Forms.Button btnai;
+        private System.Windows.Forms.Button btnkash;
+        private System.Windows.Forms.Button btnjan;
+        private System.Windows.Forms.Button btnloc;
+        private System.Windows.Forms.Button btnape;
+        private System.Windows.Forms.Button btnmoon;
     }
 }
--- EventManagement/Movies.cs
+++ EventManagement/Movies.cs
@@ -16,5 +16,10 @@ namespace EventManagement
         {
             InitializeComponent();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
