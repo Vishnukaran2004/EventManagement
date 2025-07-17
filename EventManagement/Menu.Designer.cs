ca55efe622b3d082d88a7c7597bef39f00a9cfac
Vishnukaran2004 <kkvishnukaran24680@gmail.com>
2025-07-17 23:49:08 +0530

    Menu

--- EventManagement/Menu.Designer.cs
+++ EventManagement/Menu.Designer.cs
@@ -33,10 +33,18 @@
             this.moviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.concertsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.paymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.profileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btncam = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnani = new System.Windows.Forms.Button();
+            this.btncyum = new System.Windows.Forms.Button();
+            this.btnmushroom = new System.Windows.Forms.Button();
+            this.btnslban = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -49,7 +57,7 @@
             this.aboutUsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(894, 30);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -59,7 +67,7 @@
             this.moviesToolStripMenuItem,
             this.concertsToolStripMenuItem});
             this.eventsToolStripMenuItem.Name = "eventsToolStripMenuItem";
-            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
             this.eventsToolStripMenuItem.Text = "Events";
             // 
             // moviesToolStripMenuItem
@@ -67,52 +75,127 @@
             this.moviesToolStripMenuItem.Name = "moviesToolStripMenuItem";
             this.moviesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.moviesToolStripMenuItem.Text = "Movies";
+            this.moviesToolStripMenuItem.Click += new System.EventHandler(this.moviesToolStripMenuItem_Click);
             // 
             // concertsToolStripMenuItem
             // 
             this.concertsToolStripMenuItem.Name = "concertsToolStripMenuItem";
             this.concertsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.concertsToolStripMenuItem.Text = "Concerts";
+            this.concertsToolStripMenuItem.Click += new System.EventHandler(this.concertsToolStripMenuItem_Click);
             // 
             // paymentToolStripMenuItem
             // 
             this.paymentToolStripMenuItem.Name = "paymentToolStripMenuItem";
-            this.paymentToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.paymentToolStripMenuItem.Size = new System.Drawing.Size(79, 26);
             this.paymentToolStripMenuItem.Text = "Payment";
-            // 
-            // aboutUsToolStripMenuItem
-            // 
-            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
-            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
-            this.aboutUsToolStripMenuItem.Text = "About us";
+            this.paymentToolStripMenuItem.Click += new System.EventHandler(this.paymentToolStripMenuItem_Click);
             // 
             // profileToolStripMenuItem
             // 
             this.profileToolStripMenuItem.Name = "profileToolStripMenuItem";
-            this.profileToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.profileToolStripMenuItem.Size = new System.Drawing.Size(66, 26);
             this.profileToolStripMenuItem.Text = "Profile";
+            this.profileToolStripMenuItem.Click += new System.EventHandler(this.profileToolStripMenuItem_Click);
             // 
-            // button1
+            // aboutUsToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(156, 149);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 129);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(82, 26);
+            this.aboutUsToolStripMenuItem.Text = "About us";
+            this.aboutUsToolStripMenuItem.Click += new System.EventHandler(this.aboutUsToolStripMenuItem_Click);
+            // 
+            // btncam
+            // 
+            this.btncam.Location = new System.Drawing.Point(133, 54);
+            this.btncam.Name = "btncam";
+            this.btncam.Size = new System.Drawing.Size(122, 129);
+            this.btncam.TabIndex = 1;
+            this.btncam.Text = "Cammathar";
+            this.btncam.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btncam.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnmushroom);
+            this.groupBox1.Controls.Add(this.btncyum);
+            this.groupBox1.Controls.Add(this.btncam);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(894, 240);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Movies";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnslban);
+            this.groupBox2.Controls.Add(this.btnani);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 278);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(894, 240);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Events";
+            // 
+            // btnani
+            // 
+            this.btnani.Location = new System.Drawing.Point(133, 37);
+            this.btnani.Name = "btnani";
+            this.btnani.Size = new System.Drawing.Size(122, 129);
+            this.btnani.TabIndex = 1;
+            this.btnani.Text = "Aniruth Concert";
+            this.btnani.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnani.UseVisualStyleBackColor = true;
+            // 
+            // btncyum
+            // 
+            this.btncyum.Location = new System.Drawing.Point(382, 54);
+            this.btncyum.Name = "btncyum";
+            this.btncyum.Size = new System.Drawing.Size(122, 129);
+            this.btncyum.TabIndex = 2;
+            this.btncyum.Text = "C-Yum";
+            this.btncyum.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btncyum.UseVisualStyleBackColor = true;
+            // 
+            // btnmushroom
+            // 
+            this.btnmushroom.Location = new System.Drawing.Point(655, 54);
+            this.btnmushroom.Name = "btnmushroom";
+            this.btnmushroom.Size = new System.Drawing.Size(122, 129);
+            this.btnmushroom.TabIndex = 3;
+            this.btnmushroom.Text = "Mushroom";
+            this.btnmushroom.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnmushroom.UseVisualStyleBackColor = true;
+            // 
+            // btnslban
+            // 
+            this.btnslban.Location = new System.Drawing.Point(382, 37);
+            this.btnslban.Name = "btnslban";
+            this.btnslban.Size = new System.Drawing.Size(122, 129);
+            this.btnslban.TabIndex = 2;
+            this.btnslban.Text = "SL vs Ban T20";
+            this.btnslban.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnslban.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(894, 524);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "Menu";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +210,12 @@
         private System.Windows.Forms.ToolStripMenuItem paymentToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem profileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btncam;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnmushroom;
+        private System.Windows.Forms.Button btncyum;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnslban;
+        private System.Windows.Forms.Button btnani;
     }
 }
--- EventManagement/Menu.cs
+++ EventManagement/Menu.cs
@@ -16,5 +16,40 @@ namespace EventManagement
         {
             InitializeComponent();
         }
+
+        private void moviesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Movies movies = new Movies();
+            movies.Show();
+        }
+
+        private void concertsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Concerts concerts = new Concerts(); 
+            concerts.Show();
+        }
+
+        private void paymentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();    
+            Payment payment = new Payment();
+            payment.Show();
+        }
+
+        private void profileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmProfile profile = new frmProfile();
+            profile.Show();
+        }
+
+        private void aboutUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AboutUs aboutUs = new AboutUs();
+            aboutUs.Show();
+        }
     }
 }
