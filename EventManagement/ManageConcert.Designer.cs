c9e44ece7cd71dc12505802118b787b2789c30c0
Vishnukaran2004 <kkvishnukaran24680@gmail.com>
2025-07-19 18:36:13 +0530

    manage movie and concert

--- EventManagement/ManageConcert.Designer.cs
+++ EventManagement/ManageConcert.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.txtyear = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtgenre = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.btnback = new System.Windows.Forms.Button();
             this.btnremove = new System.Windows.Forms.Button();
             this.btnupdate = new System.Windows.Forms.Button();
             this.btnadd = new System.Windows.Forms.Button();
+            this.txtvenue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.txttitle = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,42 +51,6 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 21;
-            // 
-            // txtyear
-            // 
-            this.txtyear.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtyear.Location = new System.Drawing.Point(193, 178);
-            this.txtyear.Name = "txtyear";
-            this.txtyear.Size = new System.Drawing.Size(151, 34);
-            this.txtyear.TabIndex = 20;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(97, 178);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 26);
-            this.label3.TabIndex = 19;
-            this.label3.Text = "Year";
-            // 
-            // txtgenre
-            // 
-            this.txtgenre.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtgenre.Location = new System.Drawing.Point(193, 116);
-            this.txtgenre.Name = "txtgenre";
-            this.txtgenre.Size = new System.Drawing.Size(151, 34);
-            this.txtgenre.TabIndex = 18;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(97, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 26);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "Genre";
             // 
             // btnback
             // 
@@ -129,23 +93,59 @@
             this.btnadd.Text = "Add";
             this.btnadd.UseVisualStyleBackColor = true;
             // 
+            // txtvenue
+            // 
+            this.txtvenue.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvenue.Location = new System.Drawing.Point(190, 197);
+            this.txtvenue.Name = "txtvenue";
+            this.txtvenue.Size = new System.Drawing.Size(151, 34);
+            this.txtvenue.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 26);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Venue";
+            // 
+            // txtdate
+            // 
+            this.txtdate.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdate.Location = new System.Drawing.Point(190, 135);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(151, 34);
+            this.txtdate.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(94, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 26);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Date";
+            // 
             // txttitle
             // 
             this.txttitle.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txttitle.Location = new System.Drawing.Point(193, 57);
+            this.txttitle.Location = new System.Drawing.Point(190, 76);
             this.txttitle.Name = "txttitle";
             this.txttitle.Size = new System.Drawing.Size(151, 34);
-            this.txttitle.TabIndex = 12;
+            this.txttitle.TabIndex = 23;
             // 
-            // label1
+            // label6
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(97, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 26);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Title";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(94, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 26);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Title";
             // 
             // ManageConcert
             // 
@@ -153,17 +153,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtvenue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txttitle);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txtyear);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtgenre);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnback);
             this.Controls.Add(this.btnremove);
             this.Controls.Add(this.btnupdate);
             this.Controls.Add(this.btnadd);
-            this.Controls.Add(this.txttitle);
-            this.Controls.Add(this.label1);
             this.Name = "ManageConcert";
             this.Text = "ManageConcert";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -175,15 +175,15 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox txtyear;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtgenre;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnback;
         private System.Windows.Forms.Button btnremove;
         private System.Windows.Forms.Button btnupdate;
         private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TextBox txtvenue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtdate;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txttitle;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
     }
 }
--- EventManagement/Managemovie.Designer.cs
+++ EventManagement/Managemovie.Designer.cs
@@ -28,37 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txttitle = new System.Windows.Forms.TextBox();
             this.btnadd = new System.Windows.Forms.Button();
             this.btnupdate = new System.Windows.Forms.Button();
             this.btnremove = new System.Windows.Forms.Button();
             this.btnback = new System.Windows.Forms.Button();
-            this.txtdate = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtvenue = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtgenre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Title";
-            // 
-            // txttitle
-            // 
-            this.txttitle.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txttitle.Location = new System.Drawing.Point(134, 51);
-            this.txttitle.Name = "txttitle";
-            this.txttitle.Size = new System.Drawing.Size(151, 34);
-            this.txttitle.TabIndex = 1;
             // 
             // btnadd
             // 
@@ -101,51 +83,69 @@
             this.btnback.UseVisualStyleBackColor = true;
             this.btnback.Click += new System.EventHandler(this.btnback_Click);
             // 
-            // txtdate
-            // 
-            this.txtdate.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtdate.Location = new System.Drawing.Point(134, 110);
-            this.txtdate.Name = "txtdate";
-            this.txtdate.Size = new System.Drawing.Size(151, 34);
-            this.txtdate.TabIndex = 7;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(38, 110);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 26);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Date";
-            // 
-            // txtvenue
-            // 
-            this.txtvenue.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtvenue.Location = new System.Drawing.Point(134, 172);
-            this.txtvenue.Name = "txtvenue";
-            this.txtvenue.Size = new System.Drawing.Size(151, 34);
-            this.txtvenue.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 172);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 26);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Venue";
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(384, 92);
+            this.dataGridView1.Location = new System.Drawing.Point(451, 84);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 10;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtyear.Location = new System.Drawing.Point(158, 200);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(151, 34);
+            this.txtyear.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 26);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Year";
+            // 
+            // txtgenre
+            // 
+            this.txtgenre.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtgenre.Location = new System.Drawing.Point(158, 138);
+            this.txtgenre.Name = "txtgenre";
+            this.txtgenre.Size = new System.Drawing.Size(151, 34);
+            this.txtgenre.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 26);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Genre";
+            // 
+            // txttitle
+            // 
+            this.txttitle.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttitle.Location = new System.Drawing.Point(158, 79);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(151, 34);
+            this.txttitle.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 26);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Title";
             // 
             // Managemovie
             // 
@@ -153,17 +153,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtgenre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txttitle);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txtvenue);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtdate);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnback);
             this.Controls.Add(this.btnremove);
             this.Controls.Add(this.btnupdate);
             this.Controls.Add(this.btnadd);
-            this.Controls.Add(this.txttitle);
-            this.Controls.Add(this.label1);
             this.Name = "Managemovie";
             this.Text = "Manage Movie";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -173,17 +173,16 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txttitle;
         private System.Windows.Forms.Button btnadd;
         private System.Windows.Forms.Button btnupdate;
         private System.Windows.Forms.Button btnremove;
         private System.Windows.Forms.Button btnback;
-        private System.Windows.Forms.TextBox txtdate;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtvenue;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtgenre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label6;
     }
 }
